77b81b13dd4f258a0caaf21d6de3a8a470dc3b41
rubiyadav <rubiflyeis@gmail.com>
2023-09-16 14:51:07 +0530

    update

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1373,7 +1373,7 @@ exports.getOrders = async (req, res, next) => {
                 return res.status(200).json({ status: 200, msg: "orders of user", data: orders })
         } catch (error) {
                 console.log(error);
-                res.status(501).send({ status: 501, message: "server error.", data: {}, });
+                return res.status(501).send({ status: 501, message: "server error.", data: {}, });
         }
 };
 exports.createIngredients = async (req, res) => {
