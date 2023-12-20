5ae4cc411087ff21973348334d45acfa8f289d01
rubiyadav <rubiflyeis@gmail.com>
2023-12-20 13:07:17 +0530

    update

--- controllers/userController.js
+++ controllers/userController.js
@@ -1343,29 +1343,31 @@ const calculateCartResponse = async (cart, userId) => {
                 const data3 = await User.findOne({ _id: userId });
                 const data4 = await contact.findOne().select('name image phone email numOfReviews google mapLink map ratings -_id');
                 let totalTime = 0;
-                if (cart.services.length > 0) {
-                        for (let i = 0; i < cart.services.length; i++) {
-                                totalTime = totalTime + cart.services[i].serviceId.totalMin;
+                if (cart.toTime != (null || undefined)) {
+                        if (cart.services.length > 0) {
+                                for (let i = 0; i < cart.services.length; i++) {
+                                        totalTime = totalTime + cart.services[i].serviceId.totalMin;
+                                }
                         }
-                }
-                if (cart.AddOnservicesSchema.length > 0) {
-                        for (let i = 0; i < cart.AddOnservicesSchema.length; i++) {
-                                totalTime = totalTime + cart.AddOnservicesSchema[i].addOnservicesId.totalMin;
+                        if (cart.AddOnservicesSchema.length > 0) {
+                                for (let i = 0; i < cart.AddOnservicesSchema.length; i++) {
+                                        totalTime = totalTime + cart.AddOnservicesSchema[i].addOnservicesId.totalMin;
+                                }
                         }
+                        var dateTimeString = cart.toTime;
+                        var dateTimeObject = new Date(dateTimeString);
+                        let d = dateTimeObject.toISOString().split('T')[0];
+                        var hours1 = dateTimeObject.getUTCHours();
+                        var minutes1 = dateTimeObject.getUTCMinutes();
+                        const hours = parseInt(hours1);
+                        const minutes = parseInt(minutes1);
+                        const providedTimeInMinutes = hours * 60 + minutes;
+                        let fromTimeInMinutes = providedTimeInMinutes + totalTime + 30;
+                        const fromTime = new Date(d);
+                        fromTime.setMinutes(fromTimeInMinutes);
+                        cart.fromTime = fromTime;
+                        cart.save();
                 }
-                var dateTimeString = cart.toTime;
-                var dateTimeObject = new Date(dateTimeString);
-                let d = dateTimeObject.toISOString().split('T')[0];
-                var hours1 = dateTimeObject.getUTCHours();
-                var minutes1 = dateTimeObject.getUTCMinutes();
-                const hours = parseInt(hours1);
-                const minutes = parseInt(minutes1);
-                const providedTimeInMinutes = hours * 60 + minutes;
-                let fromTimeInMinutes = providedTimeInMinutes + totalTime + 30;
-                const fromTime = new Date(d);
-                fromTime.setMinutes(fromTimeInMinutes);
-                cart.fromTime = fromTime;
-                cart.save();
                 const cartResponse = cart.toObject();
                 let offerDiscount = 0, productFBPTotal = 0, onProductDiscount = 0, membershipDiscount = 0, shipping = 0, membershipDiscountPercentage = 0, total = 0, subTotal = 0;
                 if (cartResponse.services.length > 0) {
@@ -1564,10 +1566,12 @@ const calculateCartResponse = async (cart, userId) => {
                 cartResponse.deliveryAddresss = data2;
                 cartResponse.contactDetail = data4;
                 cartResponse.billingAddresss = data5;
-                var hours2 = Math.floor(totalTime / 60);
-                var minutes2 = totalTime % 60;
-                let timeInMin = hours2 + "hr" + " " + minutes2 + "min"
-                cartResponse.timeInMin = timeInMin;
+                if (totalTime > 0) {
+                        var hours2 = Math.floor(totalTime / 60);
+                        var minutes2 = totalTime % 60;
+                        let timeInMin = hours2 + "hr" + " " + minutes2 + "min"
+                        cartResponse.timeInMin = timeInMin;
+                }
                 return cartResponse;
         } catch (error) {
                 throw error;
